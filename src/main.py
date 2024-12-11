d901646e9f560fb0876441658ce3fe3cdcd886f3
vinhle000 <vinhle000@gmail.com>
2024-12-10 16:03:12 -0800

    Refactor file copying logic to create a clean destination directory and implement recursive content copying

--- src/main.py
+++ src/main.py
@@ -9,53 +9,33 @@ def main():
     root_dir = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
 
     src_dir = os.path.join(root_dir, "static")
-    src_paths = get_source_paths(src_dir)
-    print(' contents of src  ----> ', src_paths)
+    dest_dir = os.path.join(root_dir, "public")
+
+    # clean destination path -> Maybe put in outter function use recursion of copy_content inside
+    if os.path.exists(dest_dir):
+        shutil.rmtree(dest_dir)
+
+    copy_content(src_dir, dest_dir)
 
 
-# Write a recursive function that copies all the contents from a source directory to a destination directory (in our case, static to public)
-# It should first delete all the contents of the destination directory to ensure that the copy is clean.
+def copy_content(src, dest):
+    if os.path.isfile(src):
+        shutil.copy(src, dest)
+        return
 
-# It should copy all files and subdirectories, nested files, etc.
+    if not os.path.exists(dest):
+        os.mkdir(dest)
 
-# I recommend logging the path of each file you copy, so you can see what's happening as you run and debug your code.
-
-# Here are some of the standard library docs that might be helpful:
-# os.path.exists
-# os.listdir
-# os.path.join
-# os.path.isfile
-# os.mkdir
-# shutil.copy
-# shutil.rmtree
-
-# [ ] Hook the function up to your main function and test it out. I didn't use a unit test for this one because it interacts with the file system: I just tested it manually.
-# [ ] Add the public/ directory to your .gitignore file. This is where the generated site will live. As a general rule, it's bad to commit generated stuff, especially if it can be regenerated easily.
-# [ ] Ensure that running main.sh generates the public directory and all the copied content correctly.
-
-def copy_contents_to_dir(src, dest):
-    pass
-    # get a list of all the files and dirs from source
-
-    # for path in source_paths:
-        # new_path = "root/distination/"
-
-        # a
-
-def get_source_paths(src):
-    src_paths = []
     dir_paths = os.listdir(src)
-    print('--------')
-    print('dir paths list', dir_paths)
-    print(' ')
-    for path in dir_paths:
-        full_path = os.path.join(src, path)
-        if os.path.isfile(full_path):
-            src_paths.append(full_path)
-        else:
-            src_paths.extend(get_source_paths(full_path))
 
-    return src_paths
+    for path in dir_paths:
+        src_path = os.path.join(src, path)
+        dest_path = os.path.join(dest, path)
+        copy_content(src_path, dest_path)
+
+    return
+
+
 
 
 main()
